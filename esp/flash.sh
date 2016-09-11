fe9691192b841cf923c2b32d3873bfc9dd406c5f
mark <mark.robson@kaspersky.com>
2016-09-11 12:10:41 +0100

    update fs image offsets for new mp release

--- esp/flash.sh
+++ esp/flash.sh
@@ -1,2 +1,5 @@
 
-esptool.py write_flash 561152 fs.img
+# Older versions of Micropython used 561152
+FS_OFFSET=589824
+
+esptool.py write_flash $FS_OFFSET fs.img
--- esp/readflash.sh
+++ esp/readflash.sh
@@ -1,2 +1,5 @@
 
-esptool.py read_flash 561152 1048576 fsout.img
+# Older versions of Micropython used 561152
+FS_OFFSET=589824
+
+esptool.py read_flash $FS_OFFSET 1048576 fsout.img
